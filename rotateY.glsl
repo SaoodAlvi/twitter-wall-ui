bc4a00e7d9a16e96c850e8c776abcc8fe9c3d187
Modood Alvi <modood.ahmad.alvi@sap.com>
2018-07-20 16:01:39 +0200

    Make it as scala.js project

--- rotateY.glsl
+++
@@ -1,9 +0,0 @@
-vec3 rotateY(vec3 vector, float theta){
-  return vec3(
-    vector.z * sin(theta) + vector.x * cos(theta),
-    vector.y,
-    vector.z * cos(theta) - vector.x * sin(theta)
-  );
-}
-
-#pragma glslify: export(rotateY)
